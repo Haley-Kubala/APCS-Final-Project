5ae2c0de1e99641d2130f171750bc7fbe725930a
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-05-04 07:59:09 -0600

    moved competitor, match, round, and tournament. java to repo

---
+++ match.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tournamentproject;
+
+/**
+ *
+ * @author Haley
+ */
+public class match {
+
+    private competitor competitor1;
+    private competitor competitor2;
+
+    public match() {
+
+    }
+
+    /**
+     * @return the competitor1
+     */
+    public competitor getCompetitor1() {
+        return competitor1;
+    }
+
+    /**
+     * @param competitor1 the competitor1 to set
+     */
+    public void setCompetitor1(competitor competitor1) {
+        this.competitor1 = competitor1;
+    }
+
+    /**
+     * @return the competitor2
+     */
+    public competitor getCompetitor2() {
+        return competitor2;
+    }
+
+    /**
+     * @param competitor2 the competitor2 to set
+     */
+    public void setCompetitor2(competitor competitor2) {
+        this.competitor2 = competitor2;
+    }
+    
+    /*
+    getWinner() method? what does match even do if it
+    doesn't determine the winner between the two competitors
+    how does round do this instead?
+    how does match even reference round if it doesn't extend it?
+    what should this toString() return? the winner?
+    
+    */
+    public String toString() {
+        String output = "";
+        return output;
+    }
+}
---
+++ round.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tournamentproject;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Haley
+ */
+public class round extends match {
+
+    private ArrayList<match> matches = new ArrayList<>();
+
+    /*
+    ArrayList<match> winners = new ArrayList<>();
+    is this the correct way to reference winners... or is it just a competitor object
+    instead of a match object? 
+    is this a list of winners from each round?
+    what class/method/whatever uses this list? 
+    does it need to be ordered?
+     */
+    public round() {
+
+    }
+
+    /**
+     * @return the matches
+     */
+    public ArrayList<match> getMatches() {
+        return matches;
+    }
+
+    /**
+     * @param matches the matches to set
+     */
+    public void setMatches(ArrayList<match> matches) {
+        this.matches = matches;
+    }
+    
+    /*
+    determineWinner() method:
+    what data type?
+    does it need user input(i think so)
+    
+    */
+    
+    /*
+    should this print out the winners list and or the matches list?
+    
+    */
+    public String toString(){
+        String output = "";
+        return output;
+    }
+
+}
